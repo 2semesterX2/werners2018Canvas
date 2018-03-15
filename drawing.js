5f3a310a1ba57412658a401f032e7693f9c8162c
Bror-Arnfast <2012.github@arnfast.dk>
2018-03-15 14:59:50 +0100

    initial commit

---
+++ drawing.js
@@ -0,0 +1,69 @@
+
+//http://www.williammalone.com/articles/create-html5-canvas-javascript-drawing-app/
+
+var context=document.getElementById("wernersCanvas").getContext("2d"); 
+
+$('#wernersCanvas').mousedown(function(e){
+  var mouseX = e.pageX - this.offsetLeft;
+  var mouseY = e.pageY - this.offsetTop;
+		
+  paint = true;
+  addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+  redraw();
+});
+
+
+$('#wernersCanvas').mousemove(function(e){
+  if(paint){
+    addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+    redraw();
+  }
+});
+
+
+$('#wernersCanvas').mouseup(function(e){
+  paint = false;
+});
+
+
+$('#wernersCanvas').mouseleave(function(e){
+  paint = false;
+});
+
+var clickX = new Array();
+var clickY = new Array();
+var clickDrag = new Array();
+var paint;
+
+function addClick(x, y, dragging)
+{
+  clickX.push(x);
+  clickY.push(y);
+  clickDrag.push(dragging);
+}
+
+
+function redraw(){
+  //context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+  
+//  context.strokeStyle = "#df4b26";
+  context.strokeStyle = "#000000";
+  context.lineJoin = "round";
+  context.lineWidth = 5;
+			
+  for(var i=0; i < clickX.length; i++) {		
+    context.beginPath();
+    if(clickDrag[i] && i){
+      context.moveTo(clickX[i-1], clickY[i-1]);
+     }else{
+       context.moveTo(clickX[i]-1, clickY[i]);
+     }
+     context.lineTo(clickX[i], clickY[i]);
+     context.closePath();
+     context.stroke();
+  }
+}
+
+
+
+
